4e4b1493eeae66544e1e2f85c7651506c31d4c87
jelenacuk <jelena.cuk.1997@gmail.com>
2018-07-11 11:20:42 +0200

    Gui za unos recepta.

--- src/view/UnosRecepataView.java
+++ src/view/UnosRecepataView.java
@@ -6,12 +6,14 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 import dataClasses.Aparat;
@@ -19,13 +21,14 @@ import dataClasses.Aplikacija;
 import dataClasses.Sastojak;
 
 
+
 public class UnosRecepataView extends JPanel{
 	
 	
-	private JLabel nazivRecepata, izborSastojaka, izborRecepata;
+	private JLabel   izborAparata;
 	private JPanel panelSastojci, panelAparati, panelDugmici;
-	private JTextField tekst;
-	private JButton objavi;
+	private JTextArea tekst;
+	private JButton dodajSastojak, dodajAparat,objavi, odustani;
 	ArrayList<JCheckBox> sastojciDugmad,aparatiDugmad;
 	HashMap<JCheckBox, Sastojak> mapaDugmeSastojak;
 	HashMap<JCheckBox, Aparat> mapaDugmeAparat;
@@ -34,32 +37,49 @@ public class UnosRecepataView extends JPanel{
 	public UnosRecepataView() {
 		
 		this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
-		nazivRecepata = new JLabel("Naziv recepta: ");
-		this.add(nazivRecepata);
-		tekst = new JTextField();
-		tekst.setPreferredSize(new Dimension(1000, 500));
-		tekst.setMaximumSize(new Dimension(1000, 500));
+		
+		this.add(Box.createRigidArea(new Dimension(0, 20)));
+		JLabel nazivRecepta = new JLabel("Naziv Recepta: ");
+		this.add(nazivRecepta);
+		JTextField naziv = new JTextField();
+		naziv.setPreferredSize(new Dimension(10, 20));
+		naziv.setMaximumSize(new Dimension(1000, 20));
+		this.add(naziv);
+		
+		this.add(Box.createRigidArea(new Dimension(0, 20)));
+		
+		JLabel nacinPripreme = new JLabel("Nacin pripreme: ");
+		this.add(nacinPripreme);
+		tekst = new JTextArea();
+		//tekst.setPreferredSize(new Dimension(1000, 800));
+		//tekst.setMaximumSize(new Dimension(1000, 800));
 		this.add(tekst);
+		
+		
 		JScrollPane scrollTekst= new JScrollPane(tekst, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 		scrollTekst.setPreferredSize(new Dimension(100, 100));
 		scrollTekst.setMaximumSize(new Dimension(1000, 100));
 		this.add(scrollTekst);
 		this.setVisible(true);
 		
-		sastojciDugmad = new ArrayList<>();
+		this.add(Box.createRigidArea(new Dimension(0, 20)));
+		
+		//IZBOR SASTOJAKA I DUGME ZA DODAVAMJE NOVIH SASTOJAKA
+		sastojciDugmad = new ArrayList<JCheckBox>();
+		mapaDugmeSastojak = new HashMap<JCheckBox, Sastojak>();
+		JPanel panelZaSastojkeIDugme = new JPanel();
+		panelZaSastojkeIDugme.setLayout(new BoxLayout(panelZaSastojkeIDugme, BoxLayout.X_AXIS));
+		
+		
 		panelSastojci = new JPanel();
 		panelSastojci.setLayout(new BoxLayout (panelSastojci,BoxLayout.PAGE_AXIS));
-		mapaDugmeAparat = new HashMap();
-		mapaDugmeSastojak = new HashMap();
-		objavi = new JButton("Objavi");
-		izborSastojaka = new JLabel("Izaberi sastojke: ");
-		
-		
+		JLabel izborSastojaka =  new JLabel("Izaberi sastojke: ");
 		this.add(izborSastojaka);
-
+		this.add(Box.createRigidArea(new Dimension(0,20)));
+		
 		JScrollPane scrollSastojci= new JScrollPane(panelSastojci, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-		scrollSastojci.setPreferredSize(new Dimension(200, 250));
-		scrollSastojci.setMaximumSize(new Dimension(200, 250));
+		scrollSastojci.setPreferredSize(new Dimension(200, 200));
+		scrollSastojci.setMaximumSize(new Dimension(200, 200));
 		for (Sastojak sastojak : Aplikacija.aplikacija.getSastojci()) {	
 			JCheckBox check = new JCheckBox(sastojak.getNaziv());
 			sastojciDugmad.add(check);
@@ -67,10 +87,52 @@ public class UnosRecepataView extends JPanel{
 			//check.addActionListener(control);
 			mapaDugmeSastojak.put(check, sastojak);
 		}
-		this.add(scrollSastojci);
-
-
-		add(objavi);
+		panelZaSastojkeIDugme.add(scrollSastojci);
+		panelZaSastojkeIDugme.add(Box.createRigidArea(new Dimension(50,0)));
+		dodajSastojak = new JButton("Dodaj sastojak");
+		panelZaSastojkeIDugme.add(dodajSastojak);
+		this.add(panelZaSastojkeIDugme);
+		
+		this.add(Box.createRigidArea(new Dimension(0, 20)));
+		
+		//IZBOR APARATA I DUGME ZA DODAVAMJE NOVIH APARATA
+		mapaDugmeAparat = new HashMap<JCheckBox, Aparat>();
+		aparatiDugmad = new ArrayList<JCheckBox>();
+		JPanel panelZaAparateIDugme = new JPanel();
+		panelZaAparateIDugme.setLayout(new BoxLayout(panelZaAparateIDugme, BoxLayout.X_AXIS));
+		
+		
+		panelAparati = new JPanel();
+		panelAparati.setLayout(new BoxLayout (panelAparati,BoxLayout.PAGE_AXIS));
+		izborAparata = new JLabel("Izaberi aparate:");
+		this.add(izborAparata);
+		this.add(Box.createRigidArea(new Dimension(0,20)));
+		JScrollPane scrollAparati = new JScrollPane(panelAparati,JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		scrollAparati.setPreferredSize(new Dimension(200, 200));
+		scrollAparati.setMaximumSize(new Dimension(200, 200));
+		for (Aparat aparat : Aplikacija.aplikacija.getAparati()) {
+			JCheckBox check = new JCheckBox(aparat.getNaziv());
+			aparatiDugmad.add(check);
+			panelAparati.add(check);
+			//check.addActionListener(control);
+			mapaDugmeAparat.put(check, aparat);
+		}
+		panelZaAparateIDugme.add(scrollAparati);
+		panelZaAparateIDugme.add(Box.createRigidArea(new Dimension(50,0)));
+		dodajAparat = new JButton("Dodaj aparat");
+		panelZaAparateIDugme.add(dodajAparat);
+		this.add(panelZaAparateIDugme);
+		
+		this.add(Box.createRigidArea(new Dimension(0, 20)));
+		panelDugmici = new JPanel();
+		panelDugmici.setLayout( new BoxLayout( panelDugmici, BoxLayout.X_AXIS));
+		objavi = new JButton("Objavi");
+		panelDugmici.add(objavi);
+		panelDugmici.add(Box.createRigidArea(new Dimension(50,0)));
+		odustani = new JButton("Odustani");
+		panelDugmici.add(odustani);
+		
+		this.add(panelDugmici);
 		//objavi.addActionListener(control);
 		
 	
