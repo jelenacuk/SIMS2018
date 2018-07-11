09349ce83e90f4cbd016c9cc128949e90e6fc69e
djordjeognjenovic97 <djordjeognjenovic997@gmail.com>
2018-07-11 19:54:13 +0200

    nista posebno

--- src/Services/AparatServis.java
+++ src/Services/AparatServis.java
@@ -10,40 +10,38 @@ import java.util.ArrayList;
 
 import dataClasses.Aparat;
 
-
 public class AparatServis {
-	
+
 	private ArrayList<Aparat> aparati;
-	
+
 	public AparatServis() {
 		this.aparati = new ArrayList<Aparat>();
 	}
-	
+
 	public void ucitaj(String nazivFajla) throws IOException {
 		BufferedReader bf = new BufferedReader(new FileReader(nazivFajla));
 		String line = "";
-		while ( (line = bf.readLine()) != null ) {
+		while ((line = bf.readLine()) != null) {
 			String[] podaci = line.split("\\|");
 			int id = Integer.parseInt(podaci[0]);
 			String naziv = podaci[1];
 			String opis = podaci[2];
-			Aparat aparat = new Aparat(id,naziv, opis);
-			
+			Aparat aparat = new Aparat(id, naziv, opis);
+
 			aparati.add(aparat);
 		}
 		bf.close();
 	}
-	
+
 	public void upisiAparate(String nazivFajla) throws IOException {
 		PrintWriter upisiAparat = new PrintWriter(new FileWriter(nazivFajla));
-		
-		for(Aparat ap : this.aparati) {
-			String strZaUpis = ap.getIdAparata() + "|" + ap.getNaziv() + "|"
-					+ ap.getOpis();
+
+		for (Aparat ap : this.aparati) {
+			String strZaUpis = ap.getIdAparata() + "|" + ap.getNaziv() + "|" + ap.getOpis();
 			upisiAparat.println(strZaUpis);
 		}
 		upisiAparat.close();
-		
+
 	}
 
 	public ArrayList<Aparat> getAparati() {
@@ -53,8 +51,5 @@ public class AparatServis {
 	public void setAparati(ArrayList<Aparat> aparati) {
 		this.aparati = aparati;
 	}
-	
-	
-	
 
 }
