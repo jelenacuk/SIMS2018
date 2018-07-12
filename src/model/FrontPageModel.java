1e37127a51900e5413d8a212d3dc4266330d5f69
djordjeognjenovic97 <djordjeognjenovic997@gmail.com>
2018-07-12 14:17:55 +0200

    Dodata neka sortiranja u FrontPageModel

--- src/model/FrontPageModel.java
+++ src/model/FrontPageModel.java
@@ -1,43 +1,81 @@
 package model;
 
-
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Observable;
 
 import dataClasses.Aparat;
+import dataClasses.Recept;
 import dataClasses.Sastojak;
+import utility.Sorter;
 
 public class FrontPageModel extends Observable {
 	private ArrayList<Sastojak> selektovaniSastojci;
 	private ArrayList<Aparat> selektovaniAparati;
-	public FrontPageModel()
-	{
+	private ArrayList<Recept> receptiPoVremenu;
+	private ArrayList<Recept> receptiPoPopularnosti;
+
+	public FrontPageModel() {
 		selektovaniSastojci = new ArrayList<>();
 		selektovaniAparati = new ArrayList<>();
 	}
+
+	public ArrayList<Recept> sortirajReceptPoVremenu(ArrayList<Recept> lista) {
+		Collections.sort(lista, new Sorter("datumObjave", -1));
+		return lista;
+	}
+
+	public ArrayList<Recept> sortirajReceptPoPopularnosti(ArrayList<Recept> lista) {
+		Collections.sort(lista, new Sorter("brojLajkova", -1));
+		return lista;
+	}
+
 	public ArrayList<Sastojak> getSelektovaniSastojci() {
 		return selektovaniSastojci;
 	}
+
 	public void setSelektovaniSastojci(ArrayList<Sastojak> selektovaniSastojci) {
 		this.selektovaniSastojci = selektovaniSastojci;
 	}
+
 	public ArrayList<Aparat> getSelektovaniAparati() {
 		return selektovaniAparati;
 	}
+
 	public void setSelektovaniAparati(ArrayList<Aparat> selektovaniAparati) {
 		this.selektovaniAparati = selektovaniAparati;
 	}
+
 	public void addSelektovanSastojak(Sastojak sastojak) {
 		selektovaniSastojci.add(sastojak);
 	}
+
 	public void addSelektovanAparat(Aparat aparat) {
 		selektovaniAparati.add(aparat);
 	}
+
 	public void removeSelektovanSastojak(Sastojak sastojak) {
 		selektovaniSastojci.remove(sastojak);
 	}
+
 	public void removeSelektovanAparat(Aparat aparat) {
 		selektovaniAparati.remove(aparat);
+	}
+
+	public ArrayList<Recept> getReceptiPoVremenu() {
+		return receptiPoVremenu;
+	}
+
+	public void setReceptiPoVremenu(ArrayList<Recept> receptiPoVremenu) {
+		this.receptiPoVremenu = receptiPoVremenu;
+	}
+
+	public ArrayList<Recept> getReceptiPoPopularnosti() {
+		return receptiPoPopularnosti;
+	}
+
+	public void setReceptiPoPopularnosti(ArrayList<Recept> receptiPoPopularnosti) {
+		this.receptiPoPopularnosti = receptiPoPopularnosti;
 	}
 
 }
--- src/utility/Sorter.java
+++ src/utility/Sorter.java
@@ -5,8 +5,8 @@ import java.util.Comparator;
 import dataClasses.Korisnik;
 import dataClasses.Recept;
 
-public class Sorter implements Comparator <Object>{
-	
+public class Sorter implements Comparator<Object> {
+
 	private String kriterijum; // za korisnika je recimo broj lajkova
 	private int smer = 1; // 1 rastuce, -1 opadajuce
 
@@ -19,26 +19,29 @@ public class Sorter implements Comparator <Object>{
 	@Override
 	public int compare(Object o1, Object o2) {
 		int retVal = 0;
-		
+
 		if (o1 != null && o2 != null && o1 instanceof Korisnik && o2 instanceof Korisnik) {
 			Korisnik k1 = (Korisnik) o1;
 			Korisnik k2 = (Korisnik) o2;
 			if (kriterijum.equals("brojLajkova")) {
 				retVal = k1.getBrojacLajkova() - k2.getBrojacLajkova();
 			}
-		}
-		else if (o1 != null && o2 != null && o1 instanceof Recept && o2 instanceof Recept) {
+		} else if (o1 != null && o2 != null && o1 instanceof Recept && o2 instanceof Recept) {
 			Recept r1 = (Recept) o1;
 			Recept r2 = (Recept) o2;
 			if (kriterijum.equals("datumObjave")) {
 				retVal = r1.getDatumObjave().compareTo(r2.getDatumObjave());
+			} else if (kriterijum.equals("brojLajkova")) {
+				retVal = r1.getBrLajkova() - r2.getBrLajkova();
 			}
 		}
-		
-		return retVal*smer;
+
+		return retVal * smer;
 	}
-	
-	/* ako je neko voljan da koristi ovo, sorter poziva na sledeci nacin:
-	   Collections.sort(nekaLista, new Sorter("brojLajkova", 1)); */
+
+	/*
+	 * ako je neko voljan da koristi ovo, sorter poziva na sledeci nacin:
+	 * Collections.sort(nekaLista, new Sorter("brojLajkova", 1));
+	 */
 
 }
