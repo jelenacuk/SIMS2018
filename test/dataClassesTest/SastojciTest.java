c58218288ecc39b86602bde612a253e7d566e990
jelenacuk <jelena.cuk.1997@gmail.com>
2018-07-15 22:31:22 +0200

    Sitnice vezane za SastojciTest.

--- test/dataClassesTest/SastojciTest.java
+++ test/dataClassesTest/SastojciTest.java
@@ -1,38 +1,50 @@
 package dataClassesTest;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import org.junit.BeforeClass;
 import org.junit.jupiter.api.Test;
 
+import dataClasses.Aparat;
 import dataClasses.Aplikacija;
 import dataClasses.Sastojak;
 
 class SastojciTest {
 
 	static int brojac;
-	
+
 	@Test
 	void ucitaj() throws IOException {
 		ArrayList<Sastojak> sastojci = Sastojak.ucitaj("./test/TestFiles/SastojciTest.txt");
 		prebroj("./test/TestFiles/SastojciTest.txt");
 		assertEquals(brojac, sastojci.size());
+		//provera atributa
+		assertEquals(0, Sastojak.getSastojci().get(0).getIdSastojka());
+		assertEquals(1, Sastojak.getSastojci().get(1).getIdSastojka());
+		assertEquals(2, Sastojak.getSastojci().get(2).getIdSastojka());
+		
+		assertEquals("Luk", Sastojak.getSastojci().get(0).getNaziv());
+		assertEquals("Krastavci", Sastojak.getSastojci().get(1).getNaziv());
+		assertEquals("Pavlaka", Sastojak.getSastojci().get(2).getNaziv());
+		
 		
 	}
 	
 	@Test
 	public void upisi() throws IOException {
-		ArrayList<Sastojak> sastojci = Sastojak.ucitaj("./test/TestFiles/SastojciTest.txt");
+		
 		Aplikacija app = new Aplikacija();
-		app.setSastojci(sastojci);
+		Sastojak.ucitaj("./test/TestFiles/SastojciTest.txt");
+		app.setSastojci(Sastojak.getSastojci());
+		
 		Sastojak.upisiSastojke("./test/TestFiles/SastojciTest.txt");
 		prebroj("./test/TestFiles/SastojciTest.txt");
 		assertEquals(brojac, Aplikacija.aplikacija.getSastojci().size());
-		
 	}
 	
 	
